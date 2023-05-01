170b72ced14bd0dee225cb8612e0bab531c3d04f
unknown <kamrulhasananik019@gmail.com>
2023-05-02 02:32:32 +0600

    server dynamic add

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const app = express();
+const express=require('express');
+const app =express();
 const cors = require('cors')
-const port = process.env.PORT || 5000;
+const port =process.env.PORT || 5000;
 app.use(cors())
 
-const Dashes = require('./data/dashes.json')
+const Dashes=require('./data/dashes.json')
 
-app.get('/', (req, res) => {
+app.get('/',(req,res)=>{
     res.send("Server is running")
 });
 
-app.get('/dashes', (req, res) => {
+app.get('/dashes',(req,res)=>{
     res.send(Dashes)
 });
 
+app.get('/dashes/:id',(req,res) =>{
+    const id = req.params.id;
+    const DashesId=Dashes.find(n=>n.id == id)
+    res.send(DashesId)
+})
 
-
-app.listen(port, () => {
-    console.log(`SERVER IS Running on port : ${port}`);
+app.listen(port,()=>{
+   console.log(`SERVER IS Running on port : ${port}`);
 })
