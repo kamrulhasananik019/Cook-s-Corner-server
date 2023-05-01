b4fc9f7723ef5948b0576d12adfddd046b05561f
unknown <kamrulhasananik019@gmail.com>
2023-05-02 02:29:30 +0600

    cors add

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const app =express();
-
+const cors = require('cors')
 const port =process.env.PORT || 5000;
-
+app.use(cors())
 
 const Dashes=require('./data/dashes.json')
 
